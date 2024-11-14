e1308c60b17b8ba540025dd6b5a78914c939d272
zoey klueh <zklueh@gmail.com>
2024-11-13 22:06:56 -0700

    fixed falling gif on homepage

--- src/homepage/Homepage.jsx
+++ src/homepage/Homepage.jsx
@@ -17,7 +17,7 @@ function Homepage({ login }) {
 
 
   return (
-    <div className="container">
+    <div className="container text-center">
       {currentUser
         ? <div className="card-body"><h3 className="heading">Find your next catch today {currentUser.username}!</h3>
         <img className="falling-gif" src="https://25.media.tumblr.com/55a1be4296cc5516f9ea9ade216e99c0/tumblr_mrdbw8H48e1sx0siio1_400.gif"></img></div>
